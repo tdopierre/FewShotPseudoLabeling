c8b51ab36dd0f24a81492f45e02dd1a2e0dc8c05
Thomas Dopierre <thomas.dopierre@gmail.com>
2019-05-27 15:08:36 +0200

    Adds logging util

---
+++ util/logging.py
@@ -0,0 +1,43 @@
+import logging
+import sys
+
+
+class Logger:
+    def __init__(self, name, logs_path=None, level=logging.INFO,
+                 log_format='%(asctime)s [%(levelname)s] %(message)s'):
+        if name in logging.Logger.manager.loggerDict:
+            self._init_from_existant(name=name)
+        else:
+            self._init_from_scratch(logs_path, name, level, log_format)
+        self._init_logging_functions()
+
+    def _init_from_existant(self, name):
+        self.logger = logging.getLogger(name)
+        self.level = self.logger.level
+
+    def _init_from_scratch(self, logs_path, name, level=logging.INFO,
+                           log_format='%(asctime)s [%(levelname)s] %(message)s'):
+        self.logs_path = logs_path
+        self.level = level
+        self.format = log_format
+
+        # Create logger and sets default level to DEBUG
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Adds a handler which logs to STDOUT
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(self.level)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def _init_logging_functions(self):
+        self.debug = self.logger.debug
+        self.info = self.logger.info
+        self.warning = self.warn = self.logger.warning
+        self.error = self.logger.error
+        self.critical = self.logger.critical
+
+    def log(self, msg):
+        self.logger.log(level=self.level, msg=msg)
