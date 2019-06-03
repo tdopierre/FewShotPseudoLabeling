7ac11b18f42b0226e734de8953b1d5e6a0f78f6f
Thomas Dopierre <thomas.dopierre@gmail.com>
2019-06-03 09:15:02 +0200

    Script now saves pseudo-labels to location specified with --output, adds args checking before running

--- get_pseudo_labels.py
+++ get_pseudo_labels.py
@@ -1,6 +1,10 @@
+import json
+import os
 import argparse
 from models.pseudo_labelling import NaiveKNNPseudoLabeler, SpectralPseudoLabeler, HierarchicalPseudoLabeler
 from models.embedders import FastTextEmbedder
+from util.data import save_data_jsonl
+import datetime
 
 
 def get_args():
@@ -10,21 +14,29 @@ def get_args():
     args_parser.add_argument('method', type=str, choices=['nKNN', 'spectral', 'hierarchical', 'aggregated'])
 
     # Language to use to embed sentences
-    args_parser.add_argument('--language', type=str, required=True)
+    args_parser.add_argument('--language', type=str, required=True, help='Language of data')
 
     # Input files, as well as file formats
-    args_parser.add_argument('--input-labeled-file', type=str, required=True)
-    args_parser.add_argument('--input-unlabeled-file', type=str, required=True)
+    args_parser.add_argument('--input-labeled-file', type=str, required=True, help='Path to labeled file')
+    args_parser.add_argument('--input-unlabeled-file', type=str, required=True, help='Path to unlabeled file')
 
+    # Output location
+    args_parser.add_argument('--output', type=str, default=f'runs/{datetime.datetime.now().isoformat()}')
     return args_parser.parse_args()
 
 
 def check_args(args):
-    pass
+    if os.path.exists(args.output):
+        raise FileExistsError(f'Path {args.output} already exists.')
+    if not os.path.exists(args.input_labeled_file):
+        raise FileNotFoundError(f'Path {args.input_labeled_file} does not exist.')
+    if not os.path.exists(args.input_unlabeled_file):
+        raise FileNotFoundError(f'Path {args.input_unlabeled_file} does not exist.')
 
 
 def main():
     args = get_args()
+    args_dict = vars(args)
     check_args(args)
 
     embedder = FastTextEmbedder(language=args.language)
@@ -40,7 +52,10 @@ def main():
         labeled_file_path=args.input_labeled_file,
         unlabeled_file_path=args.input_unlabeled_file
     )
-    print(pseudo_labels)
+    os.makedirs(args.output)
+    with open(os.path.join(args.output, 'args.json'), 'w') as file:
+        json.dump(args_dict, file, indent=2, ensure_ascii=False)
+    save_data_jsonl(pseudo_labels, os.path.join(args.output, 'pseudo_labels.jsonl'))
 
 
 if __name__ == '__main__':
